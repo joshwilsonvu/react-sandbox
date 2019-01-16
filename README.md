bf1a428f5ebc015595ffba43792a474f88dc860f
Josh Wilson <joshua.wilson@vanderbilt.edu>
2019-01-16 16:10:42 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-sandbox
+A TypeScript React component for securely running untrusted JavaScript
+in the browser.
---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+declare const Sandbox: (props: {
+    dependencies: string | string[];
+    script: string;
+}) => JSX.Element;
+export default Sandbox;
